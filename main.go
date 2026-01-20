d49b8fb1cddd2e5fae0aa995eacc388ba4878981
joao garcia <joaobernardogarcia@gmail.com>
2026-01-20 23:13:41 +0000

    initial commit and structure

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+//func main() {
+//	p := tea.NewProgram(app.NewRoot(), tea.WithAltScreen())
+//	if _, err := p.Run(); err != nil {
+//		fmt.Fprintln(os.Stderr, "error:", err)
+//		os.Exit(1)
+//	}
+//}
